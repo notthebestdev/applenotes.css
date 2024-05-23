6ce015fa0d59db71eabeed66645b88ca1530a0c0
thebestdev <thebestdeveloper@outlook.fr>
2024-05-23 21:30:18 +0200

    Update applenotes.js

--- applenotes.js
+++ applenotes.js
@@ -1,12 +1,3 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  document.querySelectorAll('ul.custom-bullets li').forEach(item => {
-    item.addEventListener('click', function() {
-      this.classList.toggle('checked');
-    });
-  });
+// applenotes.js file
 
-  // Transforming all <img> elements to have rounded corners
-  document.querySelectorAll('img').forEach(img => {
-    img.style.borderRadius = '20px'; // Sets rounded corners for images
-  });
-});
+document.addEventListener("DOMContentLoaded",e=>{document.querySelectorAll("ul.custom-bullets li").forEach(e=>{e.addEventListener("click",function(){this.classList.toggle("checked")})}),document.querySelectorAll("img").forEach(e=>{e.style.borderRadius="20px"})});
